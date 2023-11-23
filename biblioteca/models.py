cb9f1b1248bd74da2efa1fa8efca1fb07da3a0c8
sergioguijarroc <sergioguijarroc@gmail.com>
2023-11-23 21:10:01 +0100

    Añadidas claves primarias y foráneas al modelo

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -6,15 +6,22 @@ from django.core.validators import MaxValueValidator
 
 
 class Usuario(AbstractUser):
-    dni = models.CharField(max_length=9)
+    dni = models.CharField(max_length=9, primary_key=True)
     direccion = models.TextField()
     telefono = models.IntegerField(validators=[MaxValueValidator(9)])
 
 
+class Autor(models.Model):
+    nombre = models.CharField(max_length=20)
+    biogafia = models.TextField()
+    foto = models.ImageField()
+    # A autor no le asignamos clave primaria ya que django por defecto le asigna un id que es autoincremental
+
+
 class Libro(models.Model):
     isbn = models.CharField(max_length=13, primary_key=True)
-    autor = models.CharField(max_length=50)
-    editorial = models.CharField(max_length=50)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    editorial = models.ForeignKey("Editorial", on_delete=models.CASCADE)
     fecha_publicacion = models.DateField()
     genero = models.CharField(max_length=20)
     resumen = models.TextField()
@@ -22,15 +29,19 @@ class Libro(models.Model):
         ("D", "Disponible"),
         ("P", "Prestado"),
         ("E", "En proceso de préstamo"),
-    )
+    )  # Esto es una tupla de tuplas (tupla anidada) para que el usuario solo pueda elegir entre las opciones que le damos
     portada = models.ImageField()
-
-
-class Autor(models.Model):
-    nombre = models.CharField(primary_key=True)
-    biogafia = models.TextField()
-    foto = models.ImageField()
 
 
 class Editorial(models.Model):
     nombre = models.CharField(max_length=15)
+    direccion = models.TextField()
+    sitio_web = models.URLField()
+
+
+class Prestamo(models.Model):
+    libro_prestado = models.ForeignKey(Libro, on_delete=models.CASCADE)
+    fecha_prestamo = models.DateField()
+    fecha_devolucion = models.DateField()
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    estado_prestamo = models.CharField(max_length=1, choices=Libro.DISPONIBILIDAD)
